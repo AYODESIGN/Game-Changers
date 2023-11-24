59f47a9ef2822bbce34a4efd54df4553be5f4ad1
AYODESIGN <104289006+AYODESIGN@users.noreply.github.com>
2023-11-24 23:31:08 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl:'https://game-changer-8v8f.onrender.com'
+  // baseUrl:'https://game-changer-8v8f.onrender.com'
+ baseUrl:'https://game-changer0.onrender.com'
 
 };
