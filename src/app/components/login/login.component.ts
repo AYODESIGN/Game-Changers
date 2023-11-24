cdd68db30ae52b1fc618093c69c7951a41093034
AYODESIGN <104289006+AYODESIGN@users.noreply.github.com>
2023-11-24 18:16:52 +0100

    Update login.component.ts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsersService } from 'src/app/services/users.service';
-import swal from 'sweetalert';
+// import swal from 'sweetalert';
 
 
 @Component({
@@ -28,34 +28,64 @@ export class LoginComponent implements OnInit {
 
   }
 
-  login() {
-    this.usersService.login(this.loginForm.value).subscribe((response) => {
-        console.log(response.user);
-        console.log(response.msg);
+  // login() {
+  //   this.usersService.login(this.loginForm.value).subscribe((response) => {
+  //       console.log(response.user);
+  //       console.log(response.msg);
   
-        if (response.msg === "2") {
-          sessionStorage.setItem('jwt', response.user);
-          swal('Success!', 'Login successful!', 'success');
+  //       if (response.msg === "2") {
+  //         sessionStorage.setItem('jwt', response.user);
+  //         swal('Success!', 'Login successful!', 'success');
  
-          if (response.role === 'admin') {
-            console.log(response.role);
-            this.router.navigate(['/add-card']);
-          } else if (response.role === 'user') {
-            this.router.navigate(['']);
-          }
-        } else {
-          // Handle login failure case here
-          swal('Error!', 'Login failed. Please check your credentials.', 'error');
+  //         if (response.role === 'admin') {
+  //           console.log(response.role);
+  //           this.router.navigate(['/add-card']);
+  //         } else if (response.role === 'user') {
+  //           this.router.navigate(['']);
+  //         }
+  //       } else {
+  //         // Handle login failure case here
+  //         swal('Error!', 'Login failed. Please check your credentials.', 'error');
 
+  //       }
+  //     },
+  //     (error) => {
+  //       console.error('An error occurred:', error);
+  //       swal('Error!', 'An error occurred while logging in.', 'error');
+  //     }
+  //   );
+  // }  
+
+  login() {
+  this.usersService.login(this.loginForm.value).subscribe(
+    (response) => {
+      console.log(response.user);
+      console.log(response.msg);
+
+      if (response.msg === "2") {
+        sessionStorage.setItem('jwt', response.user);
+
+        // Use standard alert for success message
+        window.alert('Success! Login successful!');
+
+        if (response.role === 'admin') {
+          console.log(response.role);
+          this.router.navigate(['/add-card']);
+        } else if (response.role === 'user') {
+          this.router.navigate(['']);
         }
-      },
-      (error) => {
-        console.error('An error occurred:', error);
-        swal('Error!', 'An error occurred while logging in.', 'error');
+      } else {
+        // Use standard alert for login failure message
+        window.alert('Error! Login failed. Please check your credentials.');
       }
-    );
-  }
-  
-      
-    
+    },
+    (error) => {
+      console.error('An error occurred:', error);
+
+      // Use standard alert for general error message
+      window.alert('Error! An error occurred while logging in.');
+    }
+  );
+}
+
    }
